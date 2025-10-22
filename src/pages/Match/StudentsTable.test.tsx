b4e7d2ce2c34a98089c2e9c78cb8b9ceec4ddb86
arun <arunchavali@me.com>
2025-10-21 22:40:27 -0400

    test: table header test for students

--- src/pages/Match/StudentsTable.test.tsx
+++ src/pages/Match/StudentsTable.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen, waitFor } from "@testing-library/react";
-import { StudentsTable } from "./StudentsTable";
 import { MemoryRouter } from "react-router-dom";
 import { SWRConfig } from "swr";
+import { StudentsTable } from "./StudentsTable";
 
 const fetcher = (url: string) => fetch(url).then(res => res.json());
 
@@ -16,6 +16,23 @@ describe("StudentsTable", () => {
         await waitFor(() => {
             expect(screen.getByText("Student1")).toBeInTheDocument();
             expect(screen.getByText("Last1")).toBeInTheDocument();
-        }, { timeout: 10000 });
+        });
+    });
+
+    it("renders table headers", () => {
+        render(
+            <SWRConfig value={{ fetcher }}>
+                <MemoryRouter>
+                    <StudentsTable />
+                </MemoryRouter>
+            </SWRConfig>
+        );
+        expect(screen.getByText("First Name")).toBeInTheDocument();
+        expect(screen.getByText("Last Name")).toBeInTheDocument();
+        expect(screen.getByText("Graduation Year")).toBeInTheDocument();
+        expect(screen.getByText("Email")).toBeInTheDocument();
+        expect(screen.getByText("Phone")).toBeInTheDocument();
+        expect(screen.getByText("Home Address")).toBeInTheDocument();
+        expect(screen.getByText("High School ID")).toBeInTheDocument();
     });
 });
